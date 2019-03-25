ccd76bd296b18e494927e450eac1d7540f4e1416
kr3275 <kr3275@CSELAB02-24.bennett.edu.in>
2019-03-25 14:33:59 +0530

    New Sort

---
+++ src/NewSort.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class NewSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr= {1,24,73,40,5,6,7,25,63,74};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
